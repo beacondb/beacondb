53ed4397b7a01f026ae007a1841081c1faaf59b7
Joel Koen <mail@joelkoen.com>
2024-06-03 14:11:00 +1000

    feat: accepting submissions again

---
+++ beacondb/src/geosubmit.rs
@@ -0,0 +1,83 @@
+use actix_web::{
+    error::{ErrorBadRequest, ErrorInternalServerError},
+    http::{header::USER_AGENT, StatusCode},
+    post, web, HttpRequest, HttpResponse, Responder,
+};
+use anyhow::Context;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use sqlx::{query, PgPool};
+
+// only the bare minimum is parsed here: it is assumed that certain data issues
+// may be due to device manufacturer software, making it difficult for
+// developers to handle per device.
+//
+// - https://github.com/mjaakko/NeoStumbler/issues/88
+
+#[derive(Deserialize)]
+struct Submission {
+    items: Vec<Report>,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct Report {
+    timestamp: u64,
+    position: Position,
+    #[serde(flatten)]
+    extra: Value,
+}
+
+#[derive(Deserialize, Serialize)]
+struct Position {
+    latitude: f32,
+    longitude: f32,
+    #[serde(flatten)]
+    extra: Value,
+}
+
+#[post("/v2/geosubmit")]
+pub async fn service(
+    data: web::Json<Submission>,
+    pool: web::Data<PgPool>,
+    req: HttpRequest,
+) -> actix_web::Result<impl Responder> {
+    let data = data.into_inner();
+    let pool = pool.into_inner();
+
+    let ua = match req.headers().get(USER_AGENT).map(|x| x.to_str()) {
+        Some(Ok(x)) => Some(x),
+        Some(Err(_)) => {
+            return Ok(HttpResponse::BadRequest().body("user agent contains invalid characters"))
+        }
+        None => None,
+    };
+
+    insert(&*pool, ua, data)
+        .await
+        .context("writing to database failed")
+        .map_err(ErrorInternalServerError)?;
+
+    Ok(HttpResponse::new(StatusCode::ACCEPTED))
+}
+
+async fn insert(
+    pool: &PgPool,
+    user_agent: Option<&str>,
+    submission: Submission,
+) -> anyhow::Result<()> {
+    let mut tx = pool.begin().await?;
+
+    for report in submission.items {
+        query!("insert into geosubmission (timestamp, latitude, longitude, user_agent, raw) values ($1, $2, $3, $4, $5) on conflict do nothing",
+            report.timestamp as i64,
+            report.position.latitude,
+            report.position.longitude,
+            user_agent,
+            serde_json::to_string(&report)?,
+        ).execute(&mut *tx).await?;
+    }
+
+    tx.commit().await?;
+    Ok(())
+}
--- beacondb/src/main.rs
+++ beacondb/src/main.rs
@@ -1,13 +1,56 @@
+use actix_web::{web, App, HttpServer};
 use anyhow::Result;
+use clap::{Parser, Subcommand};
 use rusqlite::Connection;
+use sqlx::PgPool;
 
+mod geosubmit;
 mod mls;
 
-fn main() -> Result<()> {
-    let mut conn = Connection::open("./beacon.db")?;
-    conn.execute_batch(include_str!("../db.sql"))?;
+#[derive(Debug, Parser)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Command,
 
-    mls::main(&mut conn)?;
+    #[arg(short, long)]
+    database_path: Option<String>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Accept new submissions over HTTP
+    Listen {
+        port: Option<u16>,
+    },
+    ImportMls,
+    Process,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Listen { port } => {
+            let pool = PgPool::connect(&dotenvy::var("DATABASE_URL")?).await?;
+
+            HttpServer::new(move || {
+                App::new()
+                    .app_data(web::Data::new(pool.clone()))
+                    .app_data(web::JsonConfig::default().limit(50 * 1024 * 1024))
+                    .service(geosubmit::service)
+            })
+            .bind(("0.0.0.0", port.unwrap_or(8080)))?
+            .run()
+            .await?;
+        }
+        _ => todo!(),
+    }
+
+    // let mut conn = Connection::open("./beacon.db")?;
+    // conn.execute_batch(include_str!("../db.sql"))?;
+
+    // mls::main(&mut conn)?;
 
     Ok(())
 }
