95336e6de84437c4acc0a29a703cd49372ee6b37
Joel Koen <mail@joelkoen.com>
2024-06-28 22:55:14 +1000

    feat: wifi geolocation from bboxes

--- src/geolocate.rs
+++ src/geolocate.rs
@@ -1,5 +1,7 @@
 use actix_web::{error::ErrorInternalServerError, post, web, HttpResponse};
-use geo::HaversineDistance;
+use geo::{
+    Area, ChamberlainDuquetteArea, HaversineDestination, HaversineDistance, HaversineIntermediate,
+};
 use mac_address::MacAddress;
 use serde::{Deserialize, Serialize};
 use sqlx::{query, query_as, MySqlPool};
@@ -71,6 +73,46 @@ pub async fn service(
 ) -> actix_web::Result<HttpResponse> {
     let data = data.into_inner();
     let pool = pool.into_inner();
+
+    let mut latw = 0.0;
+    let mut lonw = 0.0;
+    let mut rw = 0.0;
+    let mut ww = 0.0;
+    let mut c = 0;
+    for x in data.wifi_access_points {
+        let row = query_as!(
+            Bounds,
+            "select min_lat, min_lon, max_lat, max_lon from wifi where mac = ?",
+            &x.mac_address.bytes()[..]
+        )
+        .fetch_optional(&*pool)
+        .await
+        .map_err(ErrorInternalServerError)?;
+        if let Some(row) = row {
+            let (min, max) = row.points();
+            let center = (min + max) / 2.0;
+            let r = min.haversine_distance(&center);
+            let (lon, lat) = center.x_y();
+
+            if r > 500.0 {
+                continue;
+            }
+
+            let w = 1.0 / r.sqrt();
+
+            latw += lat * w;
+            lonw += lon * w;
+            rw += r * w;
+            ww += w;
+            c += 1;
+        }
+    }
+    if c > 2 {
+        latw /= ww;
+        lonw /= ww;
+        rw /= ww;
+        return Ok(HttpResponse::Ok().json(LocationResponse::new(latw, lonw, rw.min(50.0))));
+    }
 
     for x in data.cell_towers {
         let row = query_as!(Bounds,"select min_lat, min_lon, max_lat, max_lon from cell where radio = ? and country = ? and network = ? and area = ? and cell = ? and unit = ?",
