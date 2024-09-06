460b32f627cf3164501ddb0e3f13ad14f2f1132e
Joel Koen <mail@joelkoen.com>
2024-09-06 11:26:11 +1000

    build: update nix deps

--- flake.nix
+++ flake.nix
@@ -1,13 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:joelkoen/nixpkgs";
-
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:joelkoen/nixpkgs";
   };
 
   outputs = { nixpkgs, crane, flake-utils, ... }:
@@ -15,7 +10,7 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         lib = pkgs.lib;
-        craneLib = crane.lib.${system};
+        craneLib = crane.mkLib pkgs;
       in
       {
         packages.default = craneLib.buildPackage {
