e2c7324619940cb478d997e3343f18ddbb5ff8ea
Joel Koen <mail@joelkoen.com>
2024-12-10 14:32:01 +1000

    perf: don't dissolve cells
    this shouldn't be more performant, but it is

--- src/map.rs
+++ src/map.rs
@@ -3,7 +3,7 @@ use std::{collections::BTreeSet, fs, io};
 use anyhow::Result;
 use futures::TryStreamExt;
 use geo_types::MultiPolygon;
-use geojson::Geometry;
+use geojson::{FeatureCollection, Geometry};
 use h3o::{geom::dissolve, CellIndex, LatLng, Resolution};
 use sqlx::{query, query_scalar, PgPool};
 
@@ -11,18 +11,23 @@ pub const RESOLUTION: Resolution = Resolution::Eight;
 
 pub async fn run(pool: PgPool) -> Result<()> {
     let mut q = query_scalar!("select h3 from map").fetch(&pool);
-    let mut cells = Vec::new();
+    let mut features = Vec::new();
     while let Some(x) = q.try_next().await? {
         assert_eq!(x.len(), 8);
         let x: [u8; 8] = x.try_into().unwrap();
         let x = u64::from_be_bytes(x);
         let x = CellIndex::try_from(x)?;
-        cells.push(x);
+        let poly = dissolve([x])?;
+        let geom = Geometry::new((&poly).into());
+        features.push(geom.into());
     }
 
-    let poly = dissolve(cells)?;
-    let geom = Geometry::new((&poly).into());
-    println!("{}", geom.to_string());
+    let coll = FeatureCollection {
+        bbox: None,
+        features,
+        foreign_members: None,
+    };
+    println!("{}", coll.to_string());
 
     Ok(())
 }
