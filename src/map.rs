22723a65ad46de7afdf379956285b58181067046
Joel Koen <mail@joelkoen.com>
2025-02-11 17:54:12 +1000

    revert: perf: don't dissolve cells
    This reverts commit e2c7324619940cb478d997e3343f18ddbb5ff8ea.
    
    simplifies polygons in the rust side of things before passing on to tippecannoe, which doesn't seem to be simplifying them and now complains about too many features

--- src/map.rs
+++ src/map.rs
@@ -3,14 +3,14 @@ use std::{collections::BTreeSet, fs, io};
 use anyhow::Result;
 use futures::TryStreamExt;
 use geo_types::MultiPolygon;
-use geojson::{FeatureCollection, Geometry};
+use geojson::Geometry;
 use h3o::{geom::dissolve, CellIndex, LatLng, Resolution};
 use sqlx::{query, query_scalar, PgPool};
 
 pub async fn run(pool: PgPool) -> Result<()> {
     let mut tx = pool.begin().await?;
     let mut q = query_scalar!("select h3 from map").fetch(&pool);
-    let mut features = Vec::new();
+    let mut cells = Vec::new();
     while let Some(x) = q.try_next().await? {
         // query!("update map set new = false where h3 = $1", x)
         //     .execute(&mut *tx)
@@ -20,17 +20,12 @@ pub async fn run(pool: PgPool) -> Result<()> {
         let x: [u8; 8] = x.try_into().unwrap();
         let x = u64::from_be_bytes(x);
         let x = CellIndex::try_from(x)?;
-        let poly = dissolve([x])?;
-        let geom = Geometry::new((&poly).into());
-        features.push(geom.into());
+        cells.push(x);
     }
 
-    let coll = FeatureCollection {
-        bbox: None,
-        features,
-        foreign_members: None,
-    };
-    println!("{}", coll.to_string());
+    let poly = dissolve(cells)?;
+    let geom = Geometry::new((&poly).into());
+    println!("{}", geom.to_string());
 
     tx.commit().await?;
 
