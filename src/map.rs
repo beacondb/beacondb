faf1e5be0edf2202b47406f468115af00b6fd48f
AVee <github@avee.org>
2025-11-25 22:11:41 +0100

    #142: Optimize generating json data for tiles.
    Skip tiles on the antimeridian to prevent issues with tile generation.
    And no longer print the useless extra comma.

--- src/map.rs
+++ src/map.rs
@@ -17,21 +17,26 @@ use sqlx::{query_scalar, PgPool};
 pub async fn run(pool: PgPool) -> Result<()> {
     let mut q = query_scalar!("select h3 from map").fetch(&pool);
 
-    let mut first = true;
     while let Some(x) = q.try_next().await? {
         assert_eq!(x.len(), 8);
 
-        if first {
-            first = false;
-        } else {
-            print!(",");
-        }
         let x: [u8; 8] = x.try_into().unwrap();
         let x = u64::from_be_bytes(x);
         let x = CellIndex::try_from(x)?;
         let s: LineString = x.boundary().into();
-        let p = Geometry::new((&s.convex_hull()).into());
-        println!("{}", p.to_string());
+
+        // If we get a cell which crosses the antimeridian we get lines from -179.x to 179.x degrees
+        // which are then interpreted as shape that crosses across the other side of the earth. This
+        // results in horizontal lines being drawn across the map.
+        // Per geojson spec we should cut those into two shapes to prevent this from happening.
+        // See: https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.9
+        if s.lines().any(|l| l.start.x.is_sign_negative() != l.end.x.is_sign_negative() && l.start.x.abs() > 100.0) {
+            // FIXME: For now we just ignore these, but we should be splitting here.
+            continue;
+        } else {
+            let p = Geometry::new((&s.convex_hull()).into());
+            println!("{}", p.to_string());
+        }
     }
 
     Ok(())
